d674c45d74bf51351fbd146ad87f2c3eadbf07be
NicolasKonishi <145286206+NicolasKonishi@users.noreply.github.com>
2024-08-27 01:00:40 -0300

    implementando funções do recyclerView

--- app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
@@ -1,26 +1,30 @@
-package br.com.alura.orgs
+package br.com.alura.orgs.ui.activity
 
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
-import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import br.com.alura.orgs.R
+import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 
 class MainActivity : Activity() {
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
-        val nome = findViewById<TextView>(R.id.nome)
+
+        /*val nome = findViewById<TextView>(R.id.nome)
         nome.text = "Cesta de frutas"
 
         val descricao =findViewById<TextView>(R.id.descricao)
         descricao.text = "Laranja, manga e maca"
 
         val valor = findViewById<TextView>(R.id.valor)
-        valor.text = "19.99"
+        valor.text = "19.99"*/
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter = ListaProdutosAdapter()
     }
 
 }
