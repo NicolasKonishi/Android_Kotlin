7620588638c9cbbef5afb0f1c7f2ddaeac91f0dd
NicolasKonishi <145286206+NicolasKonishi@users.noreply.github.com>
2024-09-04 00:22:36 -0300

    mainactivity alterada

--- app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
@@ -3,9 +3,12 @@ package br.com.alura.orgs.ui.activity
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.alura.orgs.R
+import br.com.alura.orgs.model.Produto
 import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import java.math.BigDecimal
 
 class MainActivity : Activity() {
 
@@ -24,7 +27,26 @@ class MainActivity : Activity() {
         val valor = findViewById<TextView>(R.id.valor)
         valor.text = "19.99"*/
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.adapter = ListaProdutosAdapter()
+        recyclerView.adapter = ListaProdutosAdapter(
+            context = this, produtos = listOf(
+                Produto(
+                    nome = "teste",
+                    descricao = "bom",
+                    valor = BigDecimal("19.90")
+                ),
+                Produto(
+                    nome = "teste1",
+                    descricao = "ruim",
+                    valor = BigDecimal("10.90")
+                ),
+                Produto(
+                    nome = "teste2",
+                    descricao = "legal",
+                    valor = BigDecimal("12.90")
+                ),
+            )
+        )
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 
 }
